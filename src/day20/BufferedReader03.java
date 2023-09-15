f319a5ea440d1e37e438e132f7ed6c692d5fc620
Kim_DDos <ehdus0731@daum.net>
2023-09-15 11:50:03 +0900

    Revert "Day21 backup"
    This reverts commit 249e95293067b84b515fee424c27d615ad2ded74.

--- src/day20/BufferedReader03.java
+++ src/day20/BufferedReader03.java
@@ -1,42 +1,10 @@
 package day20;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 public class BufferedReader03 {
 
-	public static void main(String[] args) throws IOException {
-		/* BufferedReader, FileReader 를 통해서 out.txt 파일을
-		 * map에 저장하고, 합계와 총 인원수를 출력
-		 * 입력한 값을 읽어들이면 값은 전부 String
-		 * 계산을 위해서는 int 자료형으로 변환이 필요
-		 * Integer.parseInt(값) : String => int
+	public static void main(String[] args) {
+		/* 
 		 * */
-		
-		BufferedReader br = new BufferedReader(new FileReader("out.txt"));
-		HashMap<String, Integer> map = new HashMap<>();
-		int sum=0;
-		while(true) {
-			String line = br.readLine();
-			if(line==null) {
-				break;
-			}
-			String name = line.substring(0, line.indexOf(" "));
-			String num = line.substring(line.indexOf(" ")+1, line.length());
-			int score = Integer.parseInt(num);
-			map.put(name, score);
-			sum += map.get(name);
-		}
-		for(String tmp : map.keySet()) {
-			System.out.println(tmp+":"+map.get(tmp)+"점");
-			
-		}
-		System.out.println("합계:"+sum+"점, 인원수:"+map.size()+"명");
-		br.close();
 	}
 
 }
